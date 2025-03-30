db42258770f0a8fc264f69930f4aaab8f88cb59f
Thomas Lazarus <lazarustda@gmail.com>
2025-03-30 15:48:19 -0500

    Adds comment on class

--- sklx/regressor.py
+++ sklx/regressor.py
@@ -5,5 +5,9 @@ from sklx.net import NeuralNet
 
 
 class NueralNetworkRegressor(RegressorMixin, NeuralNet):
+    """
+    Implmentation of the NueralNet class. See `sklx.NeuralNet` for more details.
+    """
+
     def __init__(self, module, *args, criterion=losses.mse_loss, **kwargs):
         super().__init__(module, *args, criterion=criterion, **kwargs)
